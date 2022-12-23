d687479323d92e89d956413a5598cbadc748ff64
0xEmilio <107588482+0xEmilio@users.noreply.github.com>
2022-12-23 13:15:08 -0500

    allow for solana minting

--- src/adapters/MintAPIAdapter.ts
+++ src/adapters/MintAPIAdapter.ts
@@ -50,9 +50,10 @@ export default class MintAPIAdapter {
                     {
                         mainnet: false,
                         metadata,
-                        recipient: `${recipientInfo.chain}:${
-                            recipientInfo.value
-                        }:${recipientInfo.type === "email" ? `${recipientInfo.type}` : ""}`,
+                        recipient: `${recipientInfo.type === "email" ? `email` : ""}:
+                            ${recipientInfo.value}:
+                            ${recipientInfo.type === "email" ? `${recipientInfo.chain}` : ""}`,
+
                     },
                     {
                         "x-project-id": process.env.CROSSMINT_PROJECT_ID!,
